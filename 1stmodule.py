d629a3e5a3b1f0b71502c0a38114bd0e600d0442
scrajat2020 <65917158+scrajat2020@users.noreply.github.com>
2020-05-26 19:04:02 +0530

    test  the  module

---
+++ 1stmodule.py
@@ -0,0 +1,12 @@
+import numpy
+import operators
+
+x=int(input("enter 1st num: "))
+y=int(input("enter 2nd num: "))
+
+
+ans1=operators.add(x,y)
+ans2=operators.mult(x,y)
+
+print (ans1)
+print (ans2)
